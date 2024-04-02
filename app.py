9cdda5cff6b73e27ea2b4892c4b7374ca7600086
brandonlazcanoguzman <blazcano@terpmail.umd.edu>
2024-04-01 22:27:33 -0400

    Copied the content from the app.py lecture module and pasted it into my existing app.py file so that my file can run adequately.

--- app.py
+++ app.py
@@ -1,41 +1,32 @@
-from flask import Flask, render_template, request
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{% block page_title %}{% endblock %}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url_for('student_view_all') }}">View All Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url_for('student_create') }}">Add New Student</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-app = Flask(__name__)
+    <div class="container">
+        <h1>{% block page_head %}{% endblock %}</h1>
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Brandon Lazcano-Guzman! This is my first code change'
-
-@app.route('/hello')
-def hello():  # put application's code here
-    return render_template('hello.html')
-
-
-@app.route('/about')
-def about():  # put application's code here
-    return render_template('about.html')
-
-@app.route('/about-css')
-def aboutcss():  # put application's code here
-    return render_template('about-css.html')
-
-@app.route('/favorite-course')
-def favoritecourse():  # put application's code here
-    subject = request.args.get('subject')
-    course_number = request.args.get('course_number')
-
-    print('Subject entered: ' + request.args.get('subject'))
-    print('Course number entered: ' + request.args.get('course_number'))
-    return render_template('favorite-course.html', subject=subject, course_number=course_number)
-
-@app.route('/contact',methods=['GET', 'POST'])
-def contact():  # put application's code here
-   if request.method == 'POST':
-       return render_template('contact.html', form_submitted=True)
-   else:
-       return render_template('contact.html')
-
-
-if __name__ == '__main__':
-    app.run()
+        {% block page_content %}{% endblock %}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
